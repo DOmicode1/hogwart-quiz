57fe0c37cd40c086a7abb0fa11ae5b45a9f3943a
CaptainObjective <marcinwos18@gmail.com>
2018-08-30 22:26:05 +0200

    Patronusy-gotowe
    do poprawy jeszcze style i jakies drobiazgi

--- src/components/Patronus/Patronus.jsx
+++ src/components/Patronus/Patronus.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import { Container } from './styles';
+import Patronusy from './Patronusy';
 
 class Patronus extends React.Component {
   constructor() {
     super();
-    this.state = {
-      text: 'EXPECTO PATRONUM!',
-    };
+    this.zwierze = Patronusy[0][1];
   }
-  losuj() {
-    let losowa = Math.random();
-    console.log(losowa);
-    this.setState({ text: 'Twój Patronus to Sowa' });
-  }
+  state = {
+    text: 'EXPECTO PATRONUM!',
+  };
+  losuj = () => {
+    const i = Patronusy.length;
+    let losowa = Math.floor(Math.random() * i);
+    losowa == 0 ? (losowa = 1) : losowa; // zeby nie dalo sie sowy trafic, wywalić później
+    // console.log(Patronusy[losowa], i);
+
+    this.zwierze = Patronusy[losowa][1];
+    this.setState({ text: `Twój Patronus to ${Patronusy[losowa][0]}` });
+    // this.klik = null;
+  };
+  klik = this.losuj;
   render() {
-    console.log('test', React);
+    // let zwierze = Sowa;
+    console.log('to', this.zwierze);
     return (
-      <Container>
-        <div className="button" onClick={this.losuj}>
+      <Container name={this.zwierze}>
+        <div className="button" onClick={this.klik}>
           &nbsp;
           {this.state.text}
           &nbsp;
         </div>
+        <br />
+        <img className="obrazek" />
       </Container>
     );
   }
--- src/components/Patronus/styles.js
+++ src/components/Patronus/styles.js
@@ -1,20 +1,25 @@
 import styled from 'styled-components';
-import owl from './owl_patronus.jpg';
 
 export const Container = styled.h2`
-  padding-top: 2rem;
-  height: 100vh;
+  padding-top: 1rem;
   background-color: black;
-  background-image: url(${owl});
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: center;
+  height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  .obrazek {
+    align-self: center;
+    content: url(${props => props.name});
+    max-width: 100%;
+    height: calc(100% - 10rem);
+    border-radius: 500px;
+  }
   .button {
     background-color: var(--primary1);
     border-radius: 5px;
     width: max-content;
+    margin: 1em;
+    align-self: center;
     height: 2em;
     color: white;
     text-align: center;
